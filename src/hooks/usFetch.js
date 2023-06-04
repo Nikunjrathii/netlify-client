3b0c6133d3d4c37b5ea5f57dc09cb6088b3e81b6
palindrome1311 <mittalnaman13@gmail.com>
2023-06-04 16:50:17 -0400

    remove warning

--- src/hooks/usFetch.js
+++ src/hooks/usFetch.js
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react";
-import {fetchDataFromApi} from "../utils/api"
+import { fetchDataFromApi } from "../utils/api";
 
-const useFetch=(endpoint)=>{
-    const [data,setData]=useState()
-    useEffect(()=>{
-        makeApiCall()
-    },[endpoint])
-    const makeApiCall =async()=>{
-        const res=await fetchDataFromApi(endpoint)
-        setData(res)
-    }
-    return {data}
-}
+const useFetch = (endpoint) => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const makeApiCall = async () => {
+      const res = await fetchDataFromApi(endpoint);
+      setData(res);
+    };
+
+    makeApiCall();
+  }, [endpoint]);
+
+  return { data };
+};
+
 export default useFetch;
